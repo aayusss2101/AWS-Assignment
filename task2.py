6451c8e1a05b029bb238bfd2b195627b8ea8b3fd
Aayush Mohan Sinha <56078537+aayusss2101@users.noreply.github.com>
2023-05-29 12:09:16 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,10 +1,21 @@
-import boto3
 import json
+import os
+import boto3
 from datetime import datetime
 
+def set_environment_variables(function_name, variables):
+    client = boto3.client('lambda')
+    response = client.update_function_configuration(
+        FunctionName=function_name,
+        Environment={
+            'Variables': variables
+        }
+    )
+    print("Environment variables set successfully.")
 
-def lambda_handler(event, context):
 
+def send_data_to_s3():
+    
     BUCKET_NAME = 'aayusss2101-aws-assignment'
     now = str(datetime.now())
 
@@ -22,3 +33,21 @@ def lambda_handler(event, context):
     json_data=json.dumps(data)
 
     s3_client.put_object(Body=json_data, Bucket=BUCKET_NAME, Key=file_name)
+
+
+def lambda_handler(event, context):
+    
+    function_name=context.function_name
+    env_name='RUN_COUNT'
+    count = os.environ.get(env_name)
+    
+    if not count:
+        count=1
+        
+    count=int(count)
+    
+    if count<=3:
+        send_data_to_s3()
+    
+    set_environment_variables(function_name,{env_name: str(count+1)})
+    
